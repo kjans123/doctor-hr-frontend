d35a2a47e8983e76c2f7c5048d65ef01bd8a1539
kjans123 <kj123@duke.edu>
2018-03-30 13:40:01 -0400

    initial commit. Includes react-app folder

---
+++ doc-frontend/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello, Kyle
+      </div>
+    );
+  }
+}
+
+export default App;
