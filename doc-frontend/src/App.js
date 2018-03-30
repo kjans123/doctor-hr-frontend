b2e3485fe6160b60f3a3f9ed48c1af61c8738147
kjans123 <kj123@duke.edu>
2018-03-30 14:33:44 -0400

    added axios and material ui packages. Added first few lines of code for get button module

--- doc-frontend/src/App.js
+++ doc-frontend/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import FetchButton from './FetchButton.js'
 
 class App extends React.Component {
   render() {
---
+++ doc-frontend/src/FetchButton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import axios from 'axios';
+import RaisedButton from 'material-ui/RaisedButton';
+
+//class FetchButton extends React.Component {
+  //constructor() {
+    //super();
+    //this.state = {
+      //"data": ["Nothing"]
+    //}
+  //}
+
+  //getData = () => {
+    //axios.get("http://vcm-3594.vm.duke.edu:5000/api/heart_rate/kj123@duke.edu")
+  //}
+//}
